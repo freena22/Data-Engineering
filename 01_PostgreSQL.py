48a198d41511ccffa8d73374ff38b3736aa94294
freena <freena22@gmail.com>
2018-07-27 12:05:54 -0700

    adjust proper data types

--- 01_PostgreSQL.py
+++ 01_PostgreSQL.py
@@ -60,3 +60,131 @@ cur.execute('SELECT * FROM users')
 users = cur.fetchall()
 conn.close()
 
+
+################# Creating Tables With Proper Data Types #################
+
+
+# 1. Describing a Table
+
+conn = psycopg2.connect("dbname=dq user=dq")
+cur = conn.cursor()
+cur.execute('SELECT * FROM ign_reviews LIMIT 0')
+print(cur.description)
+
+# The desription property of the cursor outputs a tuple of column information from the table.
+
+
+""" Output
+(Column(name='id', type_code=20, display_size=None, internal_size=8, precision=None, scale=None, null_ok=None), 
+ Column(name='score_phrase', type_code=1043, display_size=None, internal_size=11, precision=None, scale=None, null_ok=None), 
+ Column(name='title', type_code=25, display_size=None, internal_size=-1, precision=None, scale=None, null_ok=None), 
+ Column(name='url', type_code=25, display_size=None, internal_size=-1, precision=None, scale=None, null_ok=None), 
+ Column(name='platform', type_code=1043, display_size=None, internal_size=20, precision=None, scale=None, null_ok=None), 
+ Column(name='score', type_code=1700, display_size=None, internal_size=2, precision=2, scale=1, null_ok=None), 
+"""
+
+# 2. Adding the Id Field With Proper Type
+
+# BIGINT type which can hold a maximum of 8 bytes, that is 64 bits 
+# Where integer onlu holds 4 bytes
+conn = psycopg2.connect("dbname=dq user=dq")
+cur = conn.cursor()
+cur.execute("""
+    CREATE TABLE ign_reviews (
+        id BIGINT PRIMARY KEY
+    )
+""")
+conn.commit()
+
+# 3. Get the Unique Content & Finding the Max length
+
+import csv
+with open('ign.csv', 'r') as f:
+    next(f)
+    reader = csv.reader(f)
+    unique_words_in_score_phrase = set([
+        row[1] for row in reader
+    ])
+
+print(unique_words_in_score_phrase)
+
+""" Output
+ {'Great', 'Mediocre', 'Bad', 'Good', 'Awful', 'Okay', 'Masterpiece', 'Amazing', 'Unbearable', 'Disaster', 'Painful'}
+ """
+
+# Finding the Max length
+
+with open('ign.csv') as f:
+    next(f)
+    reader = csv.reader(f)
+    len_characters_for_score = [
+        len(row[1]) for row in reader
+    ]
+max_score = max(len_characters_for_score)
+
+# 4. Creating the Other String Fields
+
+# DECIMAL(3, 2) [1.23, 4.59, 10.2, 100]
+​
+# DECIMAL(2, 2) [1.2, 40, 0.2]
+​
+# DECIMAL(5, 1) [100.0, 63.1, 4000.1, 55555]
+
+conn = psycopg2.connect("dbname=dq user=dq")
+cur = conn.cursor()
+
+cur.execute("""
+    CREATE TABLE ign_reviews (
+        id BIGINT PRIMARY KEY,
+        score_phrase VARCHAR(11),
+        title TEXT,
+        url TEXT,
+        platform VARCHAR(20), # set the Varchar length
+        score DECIMAL(3, 1), # set the precision
+        genre TEXT,
+        editors_choice BOOLEAN # set the boolean type
+   )
+""")
+conn.commit()
+
+
+# 5. Date Type
+
+"""
+Combine the columns into a single column using the datetime.date e.g. 2011-04-01
+
+row = ['5249979066121302517', 'Amazing', 'LittleBigPlanet PS Vita', '/games/littlebigplanet-vita/vita-98907', 
+'PlayStation Vita', '9.0', 'Platformer', 'Y', '2012', '9', '12']
+
+"""
+import csv
+from datetime import date
+cur.execute("""
+    CREATE TABLE ign_reviews (
+        id BIGINT PRIMARY KEY,
+        score_phrase VARCHAR(11),
+        title TEXT,
+        url TEXT,
+        platform VARCHAR(20),
+        score DECIMAL(3, 1),
+        genre TEXT,
+        editors_choice BOOLEAN,
+        release_date DATE
+    )
+""")
+with open('ign.csv', 'r') as f:
+    next(f)
+    reader = csv.reader(f)
+    for row in reader:
+        updated_row = row[:8]
+        updated_row.append(date(int(row[8]), int(row[9]), int(row[10])))
+        cur.execute("INSERT INTO ign_reviews VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", updated_row)
+        
+conn.commit()        
+
+
+
+
+
+
+
